378c399fbd9ed587fcb424b4caac75e90a830d36
dofroy <118687856+dofroy@users.noreply.github.com>
2022-12-13 21:26:51 +1200

    Update matrix_func.py

--- my programms/matrix_func.py
+++ my programms/matrix_func.py
@@ -12,3 +12,6 @@ def matrix(lenght=1, width=None, value=0):
     if width is None:
         return ls(lenght, lenght)
     return ls(lenght, width, value)
+
+#def matrix(n) generate matrix n*n with value = 0, def matrix can take 2 additional arguments(width and value)
+#have fun
